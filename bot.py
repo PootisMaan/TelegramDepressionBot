9ad5277ac5ec678fa456d3111b2b334c0b34c1cb
dborodin836 <dborodin836@gmail.com>
2021-12-14 14:10:46 +0200

    Added loop for questions rewrited questions

--- bot.py
+++ bot.py
@@ -9,32 +9,29 @@ result = CalculationSystem()
 
 bot = telebot.TeleBot("1991005309:AAEEhZDkMthxz6WntlUN0XA1ClpguWb9t7k")
 
-# Rewriting this question to one markup may be useful af.
 questions = ["Я нервничаю по поводу того, что раньше меня не беспокоило",
              "Я не получаю удовольствия от еды, у меня плохой аппетит",
              "Несмотря на помощь друзей и членов моей семьи, мне не удается избавиться от чувства тоски",
-             "Мне кажется, что я не хуже других",
+             "Мне кажется, что я хуже других",
              "Мне трудно сконцентрироваться на том, чем приходится заниматься",
              "Я чувствую подавленность",
              "Все, что я делаю, требует от меня дополнительных усилий",
-             "Я надеюсь на хорошее будущее",
+             "Я не надеюсь на хорошее будущее",
              "Мне кажется, что моя жизнь сложилась неудачно",
              "Я испытываю беспокойство, страхи",
              "У меня плохой ночной сон",
-             "Я чувствую себя счастливым человеком",
+             "Я чувствую себя несчастливым человеком",
              "Мне кажется, что я стал меньше говорить",
              "Меня беспокоит чувство одиночества",
              "Окружающие настроены недружелюбно ко мне",
-             "Жизнь доставляет мне удовольствие",
+             "Жизнь не доставляет мне удовольствие",
              "Я легко могу заплакать",
              "Я испытываю грусть, хандру",
              "Мне кажется, что люди меня не любят",
              "У меня нет сил и желания начинать что-либо делать", ]
 
 
-# 			Statements
-# Getting rid of global var is essential.
-def if_statement_markup1(message):
+def if_statement(message):
     global result
     if message.text == "Иногда":
         result.add_1()
@@ -44,14 +41,23 @@ def if_statement_markup1(message):
         result.add_3()
 
 
-def if_statement_markup2(message):
-    global result
-    if message.text == "Значительную часть времени":
-        result.add_1()
-    if message.text == "Иногда":
-        result.add_2()
-    if message.text == "Очень редко или никогда":
-        result.add_3()
+def question(num: int = 0):
+    current = 0
+
+    def inner():
+        nonlocal current
+        if current == 20:
+            current = 0
+        if num != -1:
+            current += 1
+            return current - 1
+        else:
+            current = 0
+
+    return inner
+
+
+get_question = question()
 
 
 # ========================   MAIN LOGIC   =============================================================================
@@ -70,142 +76,26 @@ def info(message):
     bot.register_next_step_handler(message, send_welcome)
 
 
-def get_result(message):
-    if_statement_markup1(message)
+def get_result_and_try_again(message):
+    if_statement(message)
     bot.send_message(message.chat.id, result.get_result())
-    print(result.get_result())
+    try_markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
+    btn_try_again = telebot.types.KeyboardButton('Начать снова')
+    try_markup.add(btn_try_again)
+    message = bot.send_message(message.chat.id, f"Жедаете попробовать снова?", parse_mode='html',
+                               reply_markup=try_markup)
+    bot.register_next_step_handler(message, send_welcome)
 
-
-# Add "try again" func.
-
-# ==============================================================================================================
-# Main test block. Should be redo ASAP. I think that's stupid. Add some cycles, or something... 
-# ==============================================================================================================
 
 @bot.message_handler(commands=['test'])
 def test(message):
-    result.set_to_zero()
-    question_msg = bot.send_message(message.chat.id, questions[0], reply_markup=Markups.markup1())
-    bot.register_next_step_handler(question_msg, question1)
+    if_statement(message)
+    next_question = questions[get_question()]
+    question_msg = bot.send_message(message.chat.id, next_question, reply_markup=Markups.markup1())
+    if next_question == 'У меня нет сил и желания начинать что-либо делать':
+        bot.register_next_step_handler(question_msg, get_result_and_try_again)
+    else:
+        bot.register_next_step_handler(question_msg, test)
 
-
-def question1(message):
-    if_statement_markup1(message)
-    question_msg = bot.send_message(message.chat.id, questions[1], reply_markup=Markups.markup1())
-    bot.register_next_step_handler(question_msg, question2)
-
-
-def question2(message):
-    if_statement_markup1(message)
-    question_msg = bot.send_message(message.chat.id, questions[2], reply_markup=Markups.markup1())
-    bot.register_next_step_handler(question_msg, question3)
-
-
-def question3(message):
-    if_statement_markup1(message)
-    question_msg = bot.send_message(message.chat.id, questions[3], reply_markup=Markups.markup2())
-    bot.register_next_step_handler(question_msg, question4)
-
-
-def question4(message):
-    if_statement_markup2(message)
-    question_msg = bot.send_message(message.chat.id, questions[4], reply_markup=Markups.markup1())
-    bot.register_next_step_handler(question_msg, question5)
-
-
-def question5(message):
-    if_statement_markup1(message)
-    question_msg = bot.send_message(message.chat.id, questions[5], reply_markup=Markups.markup1())
-    bot.register_next_step_handler(question_msg, question6)
-
-
-def question6(message):
-    if_statement_markup1(message)
-    question_msg = bot.send_message(message.chat.id, questions[6], reply_markup=Markups.markup1())
-    bot.register_next_step_handler(question_msg, question7)
-
-
-def question7(message):
-    if_statement_markup1(message)
-    question_msg = bot.send_message(message.chat.id, questions[7], reply_markup=Markups.markup2())
-    bot.register_next_step_handler(question_msg, question8)
-
-
-def question8(message):
-    if_statement_markup2(message)
-    question_msg = bot.send_message(message.chat.id, questions[8], reply_markup=Markups.markup1())
-    bot.register_next_step_handler(question_msg, question9)
-
-
-def question9(message):
-    question_msg = bot.send_message(message.chat.id, questions[9], reply_markup=Markups.markup1())
-    if_statement_markup1(message)
-    bot.register_next_step_handler(question_msg, question10)
-
-
-def question10(message):
-    question_msg = bot.send_message(message.chat.id, questions[10], reply_markup=Markups.markup1())
-    if_statement_markup1(message)
-    bot.register_next_step_handler(question_msg, question11)
-
-
-def question11(message):
-    question_msg = bot.send_message(message.chat.id, questions[11], reply_markup=Markups.markup2())
-    if_statement_markup1(message)
-    bot.register_next_step_handler(question_msg, question12)
-
-
-def question12(message):
-    question_msg = bot.send_message(message.chat.id, questions[12], reply_markup=Markups.markup1())
-    if_statement_markup2(message)
-    bot.register_next_step_handler(question_msg, question13)
-
-
-def question13(message):
-    question_msg = bot.send_message(message.chat.id, questions[13], reply_markup=Markups.markup1())
-    if_statement_markup1(message)
-    bot.register_next_step_handler(question_msg, question14)
-
-
-def question14(message):
-    question_msg = bot.send_message(message.chat.id, questions[14], reply_markup=Markups.markup1())
-    if_statement_markup1(message)
-    bot.register_next_step_handler(question_msg, question15)
-
-
-def question15(message):
-    question_msg = bot.send_message(message.chat.id, questions[15], reply_markup=Markups.markup2())
-    if_statement_markup1(message)
-    bot.register_next_step_handler(question_msg, question16)
-
-
-def question16(message):
-    question_msg = bot.send_message(message.chat.id, questions[16], reply_markup=Markups.markup1())
-    if_statement_markup2(message)
-    bot.register_next_step_handler(question_msg, question17)
-
-
-def question17(message):
-    question_msg = bot.send_message(message.chat.id, questions[17], reply_markup=Markups.markup1())
-    if_statement_markup1(message)
-    bot.register_next_step_handler(question_msg, question18)
-
-
-def question18(message):
-    question_msg = bot.send_message(message.chat.id, questions[18], reply_markup=Markups.markup1())
-    if_statement_markup1(message)
-    bot.register_next_step_handler(question_msg, question19)
-
-
-def question19(message):
-    question_msg = bot.send_message(message.chat.id, questions[19], reply_markup=Markups.markup1())
-    if_statement_markup1(message)
-    bot.register_next_step_handler(question_msg, get_result)
-
-
-#
-# =======================================================================================================
-#                                          End of stupid block.
-# =======================================================================================================
 
 bot.polling(none_stop=True)
--- markups.py
+++ markups.py
@@ -1,7 +1,7 @@
 from telebot import types
 
 
-class Markups():
+class Markups:
 
     def markup1():
         markup1 = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
@@ -11,15 +11,6 @@ class Markups():
         btn4 = types.KeyboardButton('Практически все время')
         markup1.add(btn1, btn2, btn3, btn4)
         return markup1
-
-    def markup2():
-        markup2 = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
-        btn1 = types.KeyboardButton('Практически все время')
-        btn2 = types.KeyboardButton('Значительную часть времени')
-        btn3 = types.KeyboardButton('Иногда')
-        btn4 = types.KeyboardButton('Очень редко или никогда')
-        markup2.add(btn1, btn2, btn3, btn4)
-        return markup2
 
     def start_markup():
         start_markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
